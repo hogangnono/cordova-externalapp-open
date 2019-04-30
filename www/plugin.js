1425769e597fb997b5207a8d14ad5a3622299fc4
Cayden <handlake@me.com>
2019-04-30 17:24:02 +0900

    안드로이드에서 수집에 오류 있는 부분 수정

--- www/plugin.js
+++ www/plugin.js
@@ -7,7 +7,8 @@ var ExternalApp = {
         exec(success, error, PLUGIN_NAME, 'openYoutube', [videoId, options || {}]);
     },
     getAppStartTime: function(success, error) {
-        exec(success, error, PLUGIN_NAME, 'getAppStartTime', {});
+        // [workaround] 파라메터가 없더라도 {} 가 아닌 [] 의 리스트 형태여야 Android 에서도 동일한 메소드로 동일 동작함
+        exec(success, error, PLUGIN_NAME, 'getAppStartTime', []);
     }
 };
 
